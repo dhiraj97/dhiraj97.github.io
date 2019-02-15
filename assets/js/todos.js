3a666040a0f87387aad4bb4f3e46dac32e91b83f
Dhiraj97 <dhirajramnani86@gmail.com>
2019-02-15 17:50:09 +0530

    Adding to do list project

---
+++ assets/js/todos.js
@@ -0,0 +1,50 @@
+
+ $("ul").on("mouseover","li",function()
+ {
+
+	 $(this).css(
+	 {
+		 fontSize:"1.5vw",
+		 color:"#000"
+	 }
+	 );
+ });
+ 
+  $("ul").on("mouseout","li",function(event)
+ {
+
+	 $(this).css(
+	 {
+		 fontSize:"1.25vw",
+		 color:"#333"
+		 
+	 }
+	 );	
+ });
+ 
+ $(".fa-pencil").on("click",function(){
+	 $("input[type='text']").fadeToggle(100);
+ });
+ 
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+$("ul").on("click","span",function(event){
+	//important method which stops bubbling of other event listeners
+	event.stopPropagation();
+	//to remove parent use $(this).parent() so li is the parent of span
+	//also if we want to animate and then remove we have to use anonymous function in fadeout so it first fades out for 1 second and then it removes this.parent(li)
+	$(this).parent().fadeOut(1000,function(){
+		$(this).remove();
+	});
+});
+$("input[type='text']").on("keypress",function(event){
+	if(event.which == 13)
+	{
+		
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-thumbs-down'></i></span>"+todoText+"</li>");
+	}
+	
+});
